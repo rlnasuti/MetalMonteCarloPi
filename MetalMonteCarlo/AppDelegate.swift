617c138263ee8c9df02de82dc8d96d595e08c906
Robert <rlnasuti@hotmail.com>
2015-11-24 17:51:54 -0700

    Initial Commit

---
+++ MetalMonteCarlo/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  MetalMonteCarlo
+//
+//  Created by Robert Nasuti on 11/24/15.
+//  Copyright © 2015 Robert Nasuti. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
